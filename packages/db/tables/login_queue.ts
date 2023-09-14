6cf7368174651443754250bd632f2c144fb3a51f
ltfei <ltfei@ltfei.cn>
2023-09-14 22:03:50 +0800

    feat(db): 定义登录状态变量

--- packages/db/tables/login_queue.ts
+++ packages/db/tables/login_queue.ts
@@ -1,6 +1,25 @@
 import { sequelize } from '../connect'
 import { DataTypes, Model } from 'sequelize'
 
+/**
+ * - 0: 未登录
+ * - 1: 已扫码
+ * - 2: 已创建登录地址
+ * - 10: 登录成功
+ * - 20: 登录失败(用户取消登录)
+ * - 21: 登录失败(其他原因)
+ * - 22: 登录超时(不在数据库体现，仅返回数据时判断时间)
+ */
+export const loginStatus = {
+  notLogin: 0,
+  scanCode: 1,
+  getQqConnectUrl: 2,
+  loginSucceed: 10,
+  loginFailedUserCancel: 20,
+  loginFailedOtherCause: 21,
+  loginFailedTimeout: 22
+}
+
 type Table = {
   id: number
   uuid: string
@@ -25,15 +44,6 @@ export const LoginQueue = sequelize.define<Model<Table, Table>, Table>(
      * todo: 增加验证域名是否合法
      */
     url: DataTypes.CHAR,
-    /**
-     * - 0: 未登录
-     * - 1: 已扫码
-     * - 2: 已创建登录地址
-     * - 10: 登录成功
-     * - 20: 登录失败(用户取消登录)
-     * - 21: 登录失败(其他原因)
-     * - 22: 登录超时(不在数据库体现，仅返回数据时判断时间)
-     */
     status: DataTypes.INTEGER,
     date: DataTypes.DATE
   },
