d951f299abab4afe425cf7adf0b0912f66b0a1d7
ltfei <ltfei@ltfei.cn>
2024-06-11 08:25:34 +0800

    feat(routers/admin/article): 获取待审核文章列表

---
+++ packages/routers/admin/article/audit.ts
@@ -0,0 +1,53 @@
+import { Router } from 'express'
+import { ArticlesAudit } from '@ltfei-blog/service-db'
+import type { Request } from '@ltfei-blog/service-app/types'
+import Joi from 'joi'
+
+const router = Router()
+
+router.post('/list', async (req: Request, res) => {
+  const body = req.validateBody<{
+    limit: number
+    offset: number
+    sortBy: string
+    sortType: 'DESC' | 'ASC'
+  }>({
+    limit: Joi.number().min(0).max(100).default(20),
+    offset: Joi.number().min(0).default(0),
+    sortBy: Joi.string().default('status'),
+    sortType: Joi.string().valid('DESC', 'ASC').default('ASC')
+  })
+
+  if (!body) {
+    return res.send({
+      status: 403
+    })
+  }
+
+  const { limit, offset, sortBy, sortType } = body
+
+  const articlesAudit = await ArticlesAudit.findAll({
+    attributes: { exclude: ['content'] },
+    limit: limit,
+    offset: offset,
+    order: [[sortBy, sortType]]
+  })
+
+  const total = await ArticlesAudit.count()
+  const unauditedCount = await ArticlesAudit.count({
+    where: {
+      status: 0
+    }
+  })
+
+  res.send({
+    status: 200,
+    data: {
+      total,
+      unauditedCount,
+      list: articlesAudit.map((e) => e.toJSON())
+    }
+  })
+})
+
+export default router
--- packages/routers/admin/article/index.ts
+++ packages/routers/admin/article/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express'
 import list from './list'
+import audit from './audit'
 import { auth, PERMISSIONS } from '@ltfei-blog/service-permission'
 
 const router = Router()
 
 router.use('/list', auth(PERMISSIONS.admin_articleList), list)
+router.use('/audit', auth(PERMISSIONS.admin_articleAudit), audit)
 
 export default router
