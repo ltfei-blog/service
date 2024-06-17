a4476b68f64aa1d9b90fd672e29c305f69464803
ltfei <ltfei@ltfei.cn>
2024-06-17 17:05:46 +0800

    feat(routers/admin/article/update): 文章修改

--- packages/routers/admin/article/index.ts
+++ packages/routers/admin/article/index.ts
@@ -8,6 +8,7 @@ import { auth, PERMISSIONS } from '@ltfei-blog/service-permission'
 const router = Router()
 
 router.use('/list', auth(PERMISSIONS.admin_articleList), list)
+router.use('/update', auth(PERMISSIONS.admin_articleUpdate), update)
 router.use('/detail', auth(PERMISSIONS.admin_articleList), detail)
 router.use('/audit', auth(PERMISSIONS.admin_articleAudit), audit)
 
--- packages/routers/admin/article/update.ts
+++ packages/routers/admin/article/update.ts
@@ -1,7 +1,30 @@
 import { Router } from 'express'
+import { Articles } from '@ltfei-blog/service-db'
+import type { Request } from '@ltfei-blog/service-app/types'
 
 const router = Router()
 
-router.post('/', (req, res) => {})
+router.post('/', async (req: Request, res) => {
+  const { id, ...data } = req.body
+  if (Number.isNaN(Number(id)) || !Number.isInteger(Number(id)) || id <= 0) {
+    return res.send({
+      status: 403
+    })
+  }
+
+  const result = await Articles.update(
+    { ...data, last_edit_time: Date.now() },
+    {
+      where: {
+        id
+      }
+    }
+  )
+
+  res.send({
+    status: 200,
+    data: result[0]
+  })
+})
 
 export default router
